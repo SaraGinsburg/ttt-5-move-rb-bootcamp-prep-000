ab851334cadfbb5b5459960736a6afcb791fb43b
Sara Ginsburg <sara.ginsburg.ny@gmail.com>
2018-02-20 15:13:08 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -7,3 +7,15 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+def input_to_index(user_input)
+  # puts "Where would you like to go?"
+  pos = user_input.strip.to_i - 1
+
+end
+
+def move(board, position = 0, chr = "X")
+  board[position] = chr
+end
+
+def get_variable_from_file(dir, fileName)
+end
